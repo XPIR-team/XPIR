d7357d520200009ae6e08e03e0c1f9530f6c8ac3
Nicolas Amat <nicolas.amat@etu.inp-toulouse.fr>
2017-05-30 10:47:13 +0200

    Add files via upload

---
+++ crypto/NFLLWESecurityEstimator/NFLLWESecurityEstimator.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+
+
+from sage.rings.real_mpfr import RRtoRR  # Import RealField from SageMath
+
+
+import numpy as np
+import os
+import re
+import datetime
+
+
+import sys
+
+# Get the current path
+pathScript = os.getcwd()
+# Initialize the path of Martin Albrecht script
+pathModule = pathScript + "/lwe-estimator"
+sys.path.append(pathModule)
+# Import lwe estimator of Martin Albrecht
+from estimator import *  
+
+
+print "Estimate the complexity of solving LWE with XPIR parameters\n"
+
+
+# Precision of 100 (high)
+RR = RealField(100)
+
+
+# Initialize the path of the NFLParams.cpp file
+pathNFLParameters = pathScript + "/NFLParams.cpp"
+
+# Chech that the file exits
+if (os.path.isfile(pathNFLParameters)):
+    
+    
+    print "Please wait...\n"
+    
+    
+    # Open the data file which contains NFL parameters
+    with open(pathNFLParameters) as paramsFile:
+        
+        # Check all lines
+        for line in paramsFile:
+
+            # Check the line that contains kMinPolyDegree
+            if 'const unsigned int kMinPolyDegree' in line:
+                
+                # Find the index of caracters before and after kMinPolyDegree
+                index1 = line.find('=')
+                index2 = line.find('\n', index1+1)
+                
+                # Set kMinPolyDegree
+                kMinPolyDegree = int(line[index1 + 2 : index2 - 1])
+            
+            # Check the line that contains kMaxPolyDegree
+            if 'const unsigned int kMaxPolyDegree' in line:
+
+                # Find the index of caracters before and after kMaxPolyDegree
+                index1 = line.find('=')
+                index2 = line.find('\n', index1+1)
+                
+                # Set kMaxPolyDegree
+                kMaxPolyDegree = int(line[index1 + 2 : index2 - 1])
+                
+            # Check the line that contains kMaxAggregatedModulusBitsize    
+            if 'const unsigned int kModulusBitsize' in line:
+
+                # Find the index of caracters before and after kModulusBitsize
+                index1 = line.find('=')
+                index2 = line.find('\n', index1+1)
+                
+                # Set kModulusBitsize
+                kModulusBitsize = int(line[index1 + 2 : index2 - 1])
+                
+            # Check the line that contains kMaxAggregatedModulusBitsize
+            if 'const unsigned int kMaxAggregatedModulusBitsize' in line:
+
+                # Find the index of caracters before and after kMaxAggregatedModulusBitsize
+                index1 = line.find('=')
+                index2 = line.find('\n', index1+1)
+                
+                # Set kMaxAggregatedModulusBitsize
+                kMaxAggregatedModulusBitsize = int(line[index1 + 2 : index2 - 1])
+     
+    
+    # Initialize the path of the NFLLWESecurityEstimated.hpp file
+    pathNFLLWESecurityEstimatedHPP = pathScript + "/../NFLLWESecurityEstimated.hpp"
+    
+    # Open NFLLWESecurityEstimated.hpp, if it does not exist, it will create it
+    paramsSecureHeader = open(pathNFLLWESecurityEstimatedHPP, 'w') 
+    
+    paramsSecureHeader.write("#include <string>\n")
+    paramsSecureHeader.write('\n')
+    paramsSecureHeader.write("using namespace std;\n")
+    paramsSecureHeader.write('\n')
+    paramsSecureHeader.write("extern string securityParameters;\n")
+
+    # Close the NFLLWESecurityEstimated.hpp file
+    paramsSecureHeader.close()
+    
+    # Initialize the path of the NFLLWESecurityEstimated.cpp file
+    pathNFLLWESecurityEstimatedCPP = pathScript + "/../NFLLWESecurityEstimated.cpp"
+    
+    # Open NFLLWESecurityEstimated.cpp, if it does not exist, it will create it
+    paramsSecure = open(pathNFLLWESecurityEstimatedCPP, 'w')
+    paramsSecure.write('#include "NFLLWESecurityEstimated.hpp"\n')
+    paramsSecure.write('\n')
+    paramsSecure.write("#include <string>\n")
+    paramsSecure.write('\n')
+    paramsSecure.write("using namespace std;\n")
+    paramsSecure.write('\n')
+    paramsSecure.write('string securityParameters = "') 
+    
+    
+    # Initialize the number of estimations
+    i =0
+    
+    # Scan n from kMinPolyDegree to kMaxPolyDegree
+    for log2n in range(int(np.log2(kMinPolyDegree)), int(np.log2(kMaxPolyDegree)) + 1, 1):
+        n = 2 ** log2n
+        
+        # Scan q from kModulusBitsize to kMaxAggregatedModulusBitsize
+        for q in range(kModulusBitsize, kMaxAggregatedModulusBitsize + 1, 60):
+            
+            # Increment the number of estimations
+            i += 1
+            
+            # Compute the number of bits for each parameters with the Martin Albrecht algortihm
+            security = estimate_lwe(n, RR(80 / RR((2 ** q)) ) , 2 ** q, skip=("mitm", "bkw", "arora-gb"))
+            # Select the security and return the number of bits
+            nbrBits = int(np.log2(min(security['sis']['bkz2'], security['dec']['bkz2'], security['kannan']['bkz2'])))
+            
+            # Write security parameters
+            paramsSecure.write(str(n))
+            paramsSecure.write(":")
+            paramsSecure.write(str(q))
+            paramsSecure.write(":")
+            paramsSecure.write(str(nbrBits))
+            paramsSecure.write('\\n')
+            
+            # Print to the user the number of the last estimation done
+            print "estimate parameters ", i, " : done"
+            
+            
+    paramsSecure.write('";\n')
+    # Close the NFLLWESecurityEstimated.cpp file
+    paramsSecure.close()
+    
+    
+    print "\nResults of the estimation written\n\nScript finished !"
+            
+            
+else:
+    # Error if XPIR file not found
+    print "ERROR : XPIR files not found !"
